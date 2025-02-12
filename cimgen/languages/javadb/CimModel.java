b95be6fe150e53afe9c5c4c9f63b626be8370d78
Thomas Günther <tom@toms-cafe.de>
2025-02-12 19:09:30 +0100

    Change the link between CimModel and BaseClass from bidirectional to unidirectional (avoid huge sql statements when handling CimModel together with BaseClass and all subclasses)
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

--- cimgen/languages/javadb/CimModel.java
+++ cimgen/languages/javadb/CimModel.java
@@ -1,21 +1,15 @@
 package cim4jdb;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * Represents the CIM model which gets stored in the database and is linked to a
@@ -30,9 +24,6 @@ public class CimModel {
     @Column(name = "cim_model_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cimModelId;
-
-    @OneToMany(mappedBy = "cimModel", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
-    private List<BaseClass> cimObjects = new ArrayList<>();
 
     /**
      * Nested repository. The implementation is automatically created.
