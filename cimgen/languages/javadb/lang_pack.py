14695183f78b9a486027de04eb9ca9780383925b
Thomas Günther <tom@toms-cafe.de>
2025-04-12 14:52:31 +0200

    Remove filtering (no longer necessary to create only a subset of classes to prevent out of memory error)
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

--- cimgen/languages/javadb/lang_pack.py
+++ cimgen/languages/javadb/lang_pack.py
@@ -70,8 +70,6 @@ def run_template(output_path: str, class_details: dict) -> None:
             else:
                 attribute["inverse_setter"] = []
 
-    if _filter_cim_classes(class_details):
-        return
     if class_details["is_a_primitive_class"] or class_details["is_a_datatype_class"]:
         return
     if class_details["is_an_enum_class"]:
@@ -169,60 +167,6 @@ def _attribute_is_primitive_string(attribute: dict) -> bool:
     return attribute["is_primitive_attribute"] and (
         attribute["attribute_class"] not in ("Float", "Decimal", "Integer", "Boolean")
     )
-
-
-def _filter_cim_classes(class_details: dict) -> bool:
-    """Filter out all cim classes that are not in cim_class_filter_list.txt
-
-    If cim_class_filter_list.txt don't exist nothing is filtered.
-    If cim_class_filter_list.txt exists only classes in the list should be created.
-    In these classes the attributes with not created attribute_classes are filtered out.
-
-    :param class_details: Dictionary with information about a class.
-    :return:              True = class is filtered out
-                          False = class not filtered out, but attributes in class_details are filtered
-    """
-    source_dir = Path(__file__).parent
-    filter_path = source_dir / "cim_class_filter_list.txt"
-    if filter_path.exists():
-        classes = set(filter_path.read_text().split())
-
-        # Check if class is filtered out
-        if not _class_ok(class_details, classes):
-            # Check for missing super classes in cim_class_filter_list.txt
-            if not classes.intersection(class_details["subclasses"]):
-                return True
-            logger.error("Superclass '{}' missing in cim_class_filter_list.txt".format(class_details["class_name"]))
-
-        # Filter attributes in class_details
-        class_details["attributes"] = list(
-            filter(lambda attr: _attribute_ok(attr, classes), class_details["attributes"])
-        )
-    return False
-
-
-def _class_ok(class_details: dict, classes: set[str]) -> bool:
-    """Check if the class is not filtered out.
-
-    :param class_details: Dictionary with information about a class.
-    :param classes:       Set of classes that should be created.
-    :return:              Class is ok?
-    """
-    return class_details["class_name"] in classes or class_details["is_an_enum_class"]
-
-
-def _attribute_ok(attribute: dict, classes: set[str]) -> bool:
-    """Check if the attribute is not filtered out.
-
-    :param attribute: Dictionary with information about an attribute.
-    :param classes:   Set of classes that should be created.
-    :return:          Attribute is ok?
-    """
-    if attribute["attribute_class"] in classes:
-        return True
-    if attribute["is_primitive_attribute"] or attribute["is_datatype_attribute"] or attribute["is_enum_attribute"]:
-        return True
-    return False
 
 
 def _attribute_is_really_used(attribute: dict) -> bool:
